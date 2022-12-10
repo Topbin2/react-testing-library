02bd39c32618f81481a8198274790c0b72416eaa
MoSangBin <mosangbin94@gmail.com>
2022-12-10 22:29:50 +0900

    testing

--- udemy-ADVANCED-REACT-TESTING-main/base-app/client/src/features/auth/components/UserProfile.test.tsx
+++ udemy-ADVANCED-REACT-TESTING-main/base-app/client/src/features/auth/components/UserProfile.test.tsx
@@ -1,8 +1,27 @@
-import { render, screen } from "@testing-library/react";
-
+import { App } from "../../../App";
+import { render, screen } from "../../../test-utils";
 import { UserProfile } from "./UserProfile";
 
+const testUser = {
+  email: "booking@google.com",
+};
+
 test("greets the user", () => {
-  render(<UserProfile />);
-  expect(screen.getByText(/hi/i)).toBeInTheDocument();
+  render(<UserProfile />, {
+    preloadedState: { user: { userDetails: testUser } },
+  });
+  expect(screen.getByText(/hi, booking@google.com/i)).toBeInTheDocument();
+});
+
+test("redirets to signin if user is falsy", () => {
+  const { history } = render(<UserProfile />);
+  expect(history.location.pathname).toBe("/signin");
+});
+
+test("view sign-in page when loading profile while not logged in", () => {
+  render(<App />, { routeHistory: ["/profile"] });
+  const heading = screen.getByRole("heading", {
+    name: /Sign in to your account/i,
+  });
+  expect(heading).toBeInTheDocument();
 });
